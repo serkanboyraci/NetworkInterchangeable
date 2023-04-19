edc6b8048f230f0cbb941a4274021d921b15d8b1
serkanboyraci <serkanboyraci@gmail.com>
2023-04-19 18:32:42 +0200

    UserModel created

---
+++ NetworkInterchangeable/Model/UserModel.swift
@@ -0,0 +1,37 @@
+//
+//  UserModel.swift
+//  NetworkInterchangeable
+//
+//  Created by Ali serkan Boyracı  on 19.04.2023.
+//
+
+import Foundation
+
+import Foundation
+
+// MARK: - UserElement
+struct User: Codable {
+    var id: Int?
+    var name, username, email: String?
+    var address: Address?
+    var phone, website: String?
+    var company: Company?
+}
+
+// MARK: - Address
+struct Address: Codable {
+    var street, suite, city, zipcode: String?
+    var geo: Geo?
+}
+
+// MARK: - Geo
+struct Geo: Codable {
+    var lat, lng: String?
+}
+
+// MARK: - Company
+struct Company: Codable {
+    var name, catchPhrase, bs: String?
+}
+
+
---
+++ NetworkInterchangeable/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  NetworkInterchangeable
+//
+//  Created by Ali serkan Boyracı  on 19.04.2023.
+//
+
+import Foundation
