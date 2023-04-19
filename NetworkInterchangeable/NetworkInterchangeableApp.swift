c79f5b3929880fb59a6dc0f53a78b5b755b671ac
serkanboyraci <serkanboyraci@gmail.com>
2023-04-19 18:23:25 +0200

    Initial Commit

---
+++ NetworkInterchangeable/NetworkInterchangeableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangeableApp.swift
+//  NetworkInterchangeable
+//
+//  Created by Ali serkan Boyracı  on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangeableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
