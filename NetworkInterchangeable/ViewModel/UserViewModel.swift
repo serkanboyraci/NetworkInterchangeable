00d8266c95b6f27a4c743c89771881e9715cdabe
serkanboyraci <serkanboyraci@gmail.com>
2023-04-25 01:46:57 +0200

    updated viewmodel

---
+++ NetworkInterchangeable/ViewModel/UserViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  UserViewModel.swift
+//  NetworkInterchangeable
+//
+//  Created by Ali serkan Boyracı  on 25.04.2023.
+//
+
+import Foundation]
+
+
+class UserListViewModel : ObservableObject {
+    @Published var userList = [UserViewModel]()
+    
+    let webservice = Webservice()
+    
+    func downloadUsers() async {
+        
+        do {
+            let users = try await webservice.download(<#T##resource: String##String#>)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+}
+
+struct UserViewModel {
+    
+    let user : User
+    let id : Int {
+        user.id
+    }
+    
+    var name : String {
+        user.username
+    }
+    
+    var emmail : String {
+        user.email
+    }
+    
+    
+}
--- NetworkInterchangeable/ViewModel/ViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewModel.swift
-//  NetworkInterchangeable
-//
-//  Created by Ali serkan Boyracı  on 19.04.2023.
-//
-
-import Foundation
