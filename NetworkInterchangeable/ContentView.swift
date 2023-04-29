27fcd3f5c0959f822a9974af380ea85d38883010
serkanboyraci <serkanboyraci@gmail.com>
2023-04-29 14:58:11 +0200

    json file added

--- NetworkInterchangeable/ContentView.swift
+++ NetworkInterchangeable/ContentView.swift
@@ -22,8 +22,15 @@ struct ContentView: View {
         List(userListViewModel.userList, id: \.id) { user in
             VStack {
                 Text(user.name)
+                    .font(.title3)
+                    .foregroundColor(.blue)
+                    .frame(maxWidth:.infinity, alignment: .leading)
                 Text(user.username)
+                    .foregroundColor(.green)
+                    .frame(maxWidth:.infinity, alignment: .leading)
                 Text(user.email)
+                    .foregroundColor(.red)
+                    .frame(maxWidth:.infinity, alignment: .leading)
             }
         }.task {
             await userListViewModel.downloadUsers()
