f0193f347efd7dcec045b8c5859c3e2f9b899163
serkanboyraci <serkanboyraci@gmail.com>
2023-05-02 23:44:54 +0200

    updated

---
+++ NetworkInterchangeable/Service/LocalService.swift
@@ -0,0 +1,19 @@
+//
+//  LocalService.swift
+//  NetworkInterchangeable
+//
+//  Created by Ali serkan Boyracı  on 1.05.2023.
+//
+
+import Foundation
+
+class LocalService {
+    
+    func download(_ resource: String) async throws -> [User] {
+        guard let path = Bundle.main.path(forResource: resource, ofType: "json") else {
+            fatalError("Resource not found.")
+        }
+        
+        let 
+    }
+}
