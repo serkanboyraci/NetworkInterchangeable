e7db5b5378878775e0e300b7304e1e9012e88129
serkanboyraci <serkanboyraci@gmail.com>
2023-05-15 23:14:19 +0200

    Update NetworkService.swift

--- NetworkInterchangeable/Service/NetworkService.swift
+++ NetworkInterchangeable/Service/NetworkService.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 protocol NetworkService {
-    func download(_ resource: String) async throws -> [User]
     
     var type : String { get }
+    
+    func download(_ resource: String) async throws -> [User]
+    
+   
 }
