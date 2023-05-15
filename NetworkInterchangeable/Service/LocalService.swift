db69dee562f523dac53f608655ef4b91aa72af22
serkanboyraci <serkanboyraci@gmail.com>
2023-05-15 18:53:59 +0200

    Local Service added

--- NetworkInterchangeable/Service/LocalService.swift
+++ NetworkInterchangeable/Service/LocalService.swift
@@ -13,5 +13,9 @@ class LocalService {
         guard let path = Bundle.main.path(forResource: resource, ofType: "json") else {
             fatalError("Resource not found.")
         }
+        
+        let data = try Data(contentsOf: URL(filePath: path))
+        
+        return try JSONDecoder().decode([User].self, from: data)
     }
 }
